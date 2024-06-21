d0a041ae8d694bef93d235d5bf2dbe31d360de1d
Sebas sabeS <104528018+sebastianMeguillanes@users.noreply.github.com>
2024-06-21 17:37:33 -0400

    Update start-minecraft.sh

--- start-minecraft.sh
+++ start-minecraft.sh
@@ -5,7 +5,8 @@ mkdir -p /data/mods
 
 # Lista de URLs de los mods (enlaces de descarga directa desde OneDrive)
 MOD_URLS=(
-  "https://onedrive.live.com/download?resid=YOUR_RESID_FOR_MOD1"
+  "
+https://jgalqq.sn.files.1drv.com/y4mM7ejJYUqZDr9eJve3Y4TQvLi5DrxCkakIpJjF-5Pt9HK28ZyGFI-AQN84Zh2K0pxbuOeVn2Sio-k0p23NpBBiGLI-we-m685AwG_ffxw18liTUgASyxTNnKStRB-d1BM-uTwMSJ6GbqTiGpY1ZnSydN-mjS4QAoJZNjs3PB4HOYyRTZRhCcIuc76VuZs0PZqG-C7iyYFHn4117DZqpgE2w"
 )
 
 # Descarga y descomprime los mods
